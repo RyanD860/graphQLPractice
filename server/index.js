e63f4e650f8324875bef7cbd4e0c27b4eb029066
RyanD860 <31831221+RyanD860@users.noreply.github.com>
2018-04-23 16:54:55 -0400

    Connection to mongo db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,6 +13,12 @@ app.use(
     graphiql: true
   })
 );
+
+// connecting to Mongoose
+mongoose.connect(process.env.CONNECTIONSTRING);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 const port = 3020;
 app.listen(port, () => {
